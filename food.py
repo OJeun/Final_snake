104c4f772a9fa20002593c068eccb1475f4b8e51
mikaem <mkmanaligod@gmail.com>
2023-03-11 16:47:10 -0800

    .

--- food.py
+++ food.py
@@ -13,24 +13,10 @@ class Food(Turtle):
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
 
-        # number_list = [0, 1, 2, 3, 4]
-        # number_set = set()
-        # while len(number_set) < 6:
-        #     number_set.add(random.randint(0, 5))
-        # self.color(random.choice(["white", "red", "yellow"]))
         self.speed("fastest")
         self.refresh()
         self.color('deep pink')
-        # self.penup()
-        # self.style = ('Courier', 30, 'italic')
-        # self.write('Hello!', align='center')
-        # self.hideturtle()
-        # self.shape("circle")
-        # self.penup()
-        # self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        # self.color("blue")
-        # self.speed("fastest")
-        # self.refresh()
+
 
     def refresh(self):
         random_x = random.randint(-280, 280)
